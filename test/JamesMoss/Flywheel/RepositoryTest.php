8df606277a2d6f91e951c8b05e2d9815e0f0bae6
James Moss <james@builtbybuffalo.com>
2014-01-15 18:28:44 +0000

    Marking locking test as incomplete

--- test/JamesMoss/Flywheel/RepositoryTest.php
+++ test/JamesMoss/Flywheel/RepositoryTest.php
@@ -76,17 +76,7 @@ class RespositoryTest extends \PHPUnit_Framework_TestCase
 
     public function testLockingOnWrite()
     {
-        $config = new Config('/tmp/flywheel');
-        $repo   = new Repository('_pages', $config);
-        $id     = 'lock_test';
-        $name   = $id . '_' . sha1($id) . '.json';
-        $path   = '/tmp/flywheel/_pages/' . $name;
-
-        $document
-
-        file_put_contents($path, '{"id": "lock_test"}');
-
-        $this->assertFalse(is_file($path));
+        $this->markTestIncomplete();
     }
 
     public function validNameProvider()
